aa8698660efe6711b8673abe8a84d5aad1b8bfb0
Adrian Paul Dimasuhid <adimasuhid07841@liceo.edu.ph>
2025-08-04 11:51:48 +0800

    labact3

---
+++ src/Book.java
@@ -0,0 +1,42 @@
+public class Book {
+    String title;
+    String author;
+    int pages;
+    Boolean isAvailable;
+
+    public Book(String title, String author, int pages) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.isAvailable = true;
+        System.out.println("A new book '" + title + "' by " + author + " has been added to the library!");
+    }
+
+    public void displayInfo() {
+        System.out.println("Title: " + title);
+        System.out.println("Author: " + author);
+        System.out.println("Pages: " + pages);
+        System.out.println("Available: " + (isAvailable ? "Yes" : "No"));
+        System.out.println();
+    }
+
+    public void borrowBook() {
+        if (isAvailable) {
+            isAvailable = false;
+            System.out.println("'" + title + "' has been borrowed.");
+        } else {
+            System.out.println("'" + title + "' is currently not available.");
+        }
+    }
+
+    public void returnBook() {
+        if (isAvailable) {
+            isAvailable = true;
+            System.out.println("'" + title + "' has been returned.");
+        } else {
+            System.out.println("'" + title + "' was not borrowed.");
+        }
+    }
+}
+
+
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,18 @@
-import java.util.Scanner;
 
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Main Branch");
+        Book book1 = new Book("A Clash of Kings", "George R. R. Martin", 761);
+        Book book2 = new Book("A Game of Thrones", "George R. R. Martin", 694);
+        Book book3 = new Book("A Storm of Swords", "George R. R. Martin", 1070);
+
+        book1.displayInfo();
+        book2.displayInfo();
+        book3.displayInfo();
+
+        book1.borrowBook();
+        book1.borrowBook();
+        book2.returnBook();
+        book3.returnBook();
     }
 }
